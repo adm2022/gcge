4de71f5f35039d6dca9be10929d8c639b0865c96
Dave Burchell <burchell@acm.org>
2022-03-08 20:13:26 -0600

    Project start - thank Matt Payne and https://start.spring.io/

---
+++ demo/src/main/java/com/example/demo/HiController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/hi", produces = MediaType.APPLICATION_JSON_VALUE)
+public class HiController {
+
+    @GetMapping
+    public String getHi() {
+        return "Hi and Howdy Hi";
+    }
+
+}
